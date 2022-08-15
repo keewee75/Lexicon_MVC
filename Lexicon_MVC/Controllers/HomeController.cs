b25e1131f5575c97e0790c8c14dcc901b2341c43
keewee75 <marko.kivi@gmail.com>
2022-08-15 13:52:28 +0200

    Add project files.

---
+++ Lexicon_MVC/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lexicon_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lexicon_MVC/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "test",
+    pattern: "test",
+    defaults: new { controller = "Home", action = "Test" });
+
+app.Run();
