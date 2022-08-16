4feb7183cc3125667c0e505259d3d34cee2f32c7
keewee75 <marko.kivi@gmail.com>
2022-08-16 16:49:29 +0200

    .

---
+++ Lexicon_MVC/Controllers/DoctorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lexicon_MVC.Controllers
+{
+    public class DoctorController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            //ViewBag.Temperature = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int Temp)
+        {
+            var cold = "°C, You are ice cold !";
+            var fever = "°C, You got a fever !";
+            var okay = "°C, You are OK !";
+            if (Temp < 37)
+            {
+                ViewBag.Temperature = Temp + cold;              
+            }
+            else if (Temp > 37)
+            {
+                ViewBag.Temperature = Temp + fever;   
+            }
+            else
+            {
+                ViewBag.Temperature = Temp + okay;               
+            }
+            return View();
+        }
+
+    }
+}
--- Lexicon_MVC/Program.cs
+++ Lexicon_MVC/Program.cs
@@ -7,28 +7,15 @@ var app = builder.Build();
 app.UseStaticFiles();
 app.UseRouting();
 
+
+// app.MapDefaultControllerRoute();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
-    name: "AboutMe",
-    pattern: "AboutMe",
-    defaults: new { controller = "Home", action = "AboutMe" });
-
-app.MapControllerRoute(
-    name: "Contact",
-    pattern: "Contact",
-    defaults: new { controller = "Home", action = "Contact" });
-
-app.MapControllerRoute(
-    name: "Projects",
-    pattern: "Projects",
-    defaults: new { controller = "Home", action = "Projects" });
-
-app.MapControllerRoute(
-    name: "test",
-    pattern: "test",
-    defaults: new { controller = "Home", action = "Test" });
+    name: "FeverCheck",
+    pattern: "FeverCheck",
+    defaults: new { controller = "Doctor", action = "Index" });
 
 app.Run();
