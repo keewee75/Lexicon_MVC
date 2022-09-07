214bc3c9b20edd34b0713c7ecd52ca228dd473a3
keewee75 <marko.kivi@gmail.com>
2022-09-07 16:01:59 +0200

    .

---
+++ Lexicon_MVC/Controllers/Cities3Controller.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lexicon_MVC.Data;
+using Lexicon_MVC.Models;
+
+namespace Lexicon_MVC.Controllers
+{
+    public class Cities3Controller : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Cities3Controller(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cities3
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Cities.Include(c => c.Country);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Cities3/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Cities == null)
+            {
+                return NotFound();
+            }
+
+            var city = await _context.Cities
+                .Include(c => c.Country)
+                .FirstOrDefaultAsync(m => m.CityId == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return View(city);
+        }
+
+        // GET: Cities3/Create
+        public IActionResult Create()
+        {
+            ViewData["CountryId"] = new SelectList(_context.Countries, "CountryId", "CountryId");
+            return View();
+        }
+
+        // POST: Cities3/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CityId,CityName,CountryId")] City city)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(city);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "CountryId", "CountryId", city.CountryId);
+            return View(city);
+        }
+
+        // GET: Cities3/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Cities == null)
+            {
+                return NotFound();
+            }
+
+            var city = await _context.Cities.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "CountryId", "CountryId", city.CountryId);
+            return View(city);
+        }
+
+        // POST: Cities3/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CityId,CityName,CountryId")] City city)
+        {
+            if (id != city.CityId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(city);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CityExists(city.CityId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "CountryId", "CountryId", city.CountryId);
+            return View(city);
+        }
+
+        // GET: Cities3/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Cities == null)
+            {
+                return NotFound();
+            }
+
+            var city = await _context.Cities
+                .Include(c => c.Country)
+                .FirstOrDefaultAsync(m => m.CityId == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return View(city);
+        }
+
+        // POST: Cities3/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Cities == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Cities'  is null.");
+            }
+            var city = await _context.Cities.FindAsync(id);
+            if (city != null)
+            {
+                _context.Cities.Remove(city);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CityExists(int id)
+        {
+          return (_context.Cities?.Any(e => e.CityId == id)).GetValueOrDefault();
+        }
+    }
+}
