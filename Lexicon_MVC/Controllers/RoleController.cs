3977834a1f25f4b7db44bc5a6067634f7644c343
keewee75 <marko.kivi@gmail.com>
2022-09-14 16:09:01 +0200

    Added function for roles.

--- Lexicon_MVC/Controllers/RoleController.cs
+++ Lexicon_MVC/Controllers/RoleController.cs
@@ -1,7 +1,9 @@
 ﻿using Lexicon_MVC.Models;
+using Lexicon_MVC.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Lexicon_MVC.Controllers
 {
@@ -20,6 +22,71 @@ namespace Lexicon_MVC.Controllers
 		public IActionResult Index()
 		{
 			return View(_roleManager.Roles);
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create (IdentityRole role) // Här behövs async, Task och await
+		{
+			role.NormalizedName = role.Name.ToUpper();
+			await _roleManager.CreateAsync(role);
+
+			return RedirectToAction("Index");
+		}
+
+		public async Task<IActionResult> Delete (string id)
+		{
+			IdentityRole role = await _roleManager.FindByIdAsync(id); // Hitta rätt roll med id
+			await _roleManager.DeleteAsync(role);
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult AddUserToRole()
+		{
+			ViewBag.Users = new SelectList(_userManager.Users, "Id", "UserName"); // Visa lista med Users
+			ViewBag.Roles = new SelectList(_roleManager.Roles, "Name", "Name"); // Visa lista med Roles
+
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddUserToRole(string userId, string roleName)
+		{
+			var user = await _userManager.FindByIdAsync(userId);
+			await _userManager.AddToRoleAsync(user, roleName);
+
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult ShowAllUsers()
+		{
+			return View(_userManager.Users);
+		}
+
+		public async Task<IActionResult> ShowUserRoles(string Id)
+		{
+			UserRoleViewModel vm = new UserRoleViewModel();
+			var user = await _userManager.FindByIdAsync(Id);
+
+			var assignedRoles = new List<string>(await _userManager.GetRolesAsync(user));
+			vm.UserId = Id;
+			vm.UserName = user.UserName;
+
+			vm.Roles.AddRange(assignedRoles);
+
+			return View(vm);
+		}
+
+		public async Task<IActionResult> RemoveRoleFromUser(string rolename, string userid)
+		{
+			var user = await _userManager.FindByIdAsync(userid);
+			await _userManager.RemoveFromRoleAsync(user, rolename);
+
+			return RedirectToAction("ShowUserRoles", new { id = userid });
 		}
 	}
 }
---
+++ Lexicon_MVC/ViewModels/UserRoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Lexicon_MVC.ViewModels
+{
+	public class UserRoleViewModel
+	{
+		public string UserId { get; set; }
+		public string UserName { get; set; }
+		public List<string> Roles { get; set; } = new List<string>();
+	}
+}
